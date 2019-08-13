e070d19094c9625a9c2fb61645fa3a4f2a68fd0c
GeorgPandeh <georgpandeh@gmail.com>
2019-08-13 16:22:00 +0300

    Updates

---
+++ precache-manifest.d05106e194904c62f0720159d15af5f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f90bb9c85f03f113a2eb42f84fe973",
+    "url": "/Chat/index.html"
+  },
+  {
+    "revision": "8cd775e324caf8e437ee",
+    "url": "/Chat/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "ec014a34d3a69e382c6c",
+    "url": "/Chat/static/css/main.6d5d24f9.chunk.css"
+  },
+  {
+    "revision": "8cd775e324caf8e437ee",
+    "url": "/Chat/static/js/2.6335925b.chunk.js"
+  },
+  {
+    "revision": "ec014a34d3a69e382c6c",
+    "url": "/Chat/static/js/main.eb2b9fa4.chunk.js"
+  },
+  {
+    "revision": "6c0716a284e20e5e162b",
+    "url": "/Chat/static/js/runtime~main.993c241a.js"
+  }
+]);
